42261e4320c74f0a885202a3c0809a3282a96134
wasitechypros-hub <wasi.techypros@gmail.com>
2026-04-28 23:35:31 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
-const express = require('express');
-const { Pool } = require('pg');
+const express = require("express")
+const { pool } = require("pg")
 const app = express();
 
 app.use(express.json());
 
-// Sirf EK BAAR pool declare karna hai
+
 const pool = new Pool({
-  connectionString: 'postgresql://neondb_owner:npg_RuloSnyX0C6W@ep-delicate-cake-amxg7dx2-pooler.c-5.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require', // Apna link yahan dalein
+  connectionString: 'postgresql://neondb_owner:npg_RuloSnyX0C6W@ep-delicate-cake-amxg7dx2-pooler.c-5.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require',
   ssl: true
 });
 
-// GET Method
+
+
 app.get('/students', async (req, res) => {
   try {
     const result = await pool.query('SELECT * FROM students');
@@ -20,8 +21,27 @@ app.get('/students', async (req, res) => {
   }
 });
 
-// Port change kar letay hain taake conflict na ho
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+
+
+app.post('/studentdata', async (req, res) => {
+  const { id, name, email } = req.body; 
+
+  try {
+
+    const result = await pool.query(
+      'INSERT INTO students (id, name, email) VALUES ($1, $2, $3) RETURNING *',
+      [id, name, email]
+    );
+    
+ 
+    res.status(201).json(result.rows[0]); 
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Database Error: " + err.message);
+  }
+});
+
+
+app.listen(3000, () => {
+  console.log("Server is running on http://localhost:3000");
 });
