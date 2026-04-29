85ac4d2a113a2d0b42791043816c883bd93a1d0a
wasitechypros-hub <wasi.techypros@gmail.com>
2026-04-28 23:42:31 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
-const express = require("express")
-const { pool } = require("pg")
+const express = require("express");
+const { Pool } = require("pg"); 
 const app = express();
 
 app.use(express.json());
 
-
 const pool = new Pool({
-  connectionString: 'postgresql://neondb_owner:npg_RuloSnyX0C6W@ep-delicate-cake-amxg7dx2-pooler.c-5.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require',
+
+  connectionString: 'postgresql://neondb_owner:npg_RuloSnyX0C6W@ep-delicate-cake-amxg7dx2-pooler.c-5.us-east-1.aws.neon.tech/neondb?sslmode=require',
   ssl: true
 });
-
 
 
 app.get('/students', async (req, res) => {
@@ -22,26 +21,17 @@ app.get('/students', async (req, res) => {
 });
 
 
-
 app.post('/studentdata', async (req, res) => {
   const { id, name, email } = req.body; 
-
   try {
-
     const result = await pool.query(
       'INSERT INTO students (id, name, email) VALUES ($1, $2, $3) RETURNING *',
       [id, name, email]
     );
-    
- 
     res.status(201).json(result.rows[0]); 
   } catch (err) {
-    console.error(err.message);
     res.status(500).send("Database Error: " + err.message);
   }
 });
 
-
-app.listen(3000, () => {
-  console.log("Server is running on http://localhost:3000");
-});
+app.listen(3000, () => console.log("Server running"));
